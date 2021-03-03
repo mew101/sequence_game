43b888ff338737a4e37849c1da43bc9ad2fc7e7c
mew101e <mshwss@gmail.com>
2021-03-03 14:48:57 -0500

    randomNumber sounds

--- game.js
+++ game.js
@@ -4,12 +4,19 @@ var gamePattern = [];
 function nextSequence(){
   var randomNumber = Math.floor(Math.random() * 4);
   var randomColor = buttonColors[randomNumber];
+  var sound = new Audio("sounds/" + randomColor + ".mp3");
+
   $('#' + randomColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  $('#' + randomColor).click(function() {
+    sound.play();
+  });
 
 
-  gamePattern.push(randomColor);
+
+
+  //gamePattern.push(randomColor);
   //console.log(gamePattern);
 
 
-}
+};
 nextSequence();

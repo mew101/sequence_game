28d787343f1c58ccf03e62a8fdf9be847f787771
mew101e <mshwss@gmail.com>
2021-03-03 12:23:06 -0500

    json flash

--- game.js
+++ game.js
@@ -4,10 +4,12 @@ var gamePattern = [];
 function nextSequence(){
   var randomNumber = Math.floor(Math.random() * 4);
   var randomColor = buttonColors[randomNumber];
+  $('#' + randomColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+
   gamePattern.push(randomColor);
-  console.log(gamePattern);
+  //console.log(gamePattern);
 
 
 }
-
 nextSequence();

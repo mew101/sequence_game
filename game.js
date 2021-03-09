a7e9a78296a180e77262a6fd131ad2a0a33a3e17
mew101e <mshwss@gmail.com>
2021-03-09 11:40:06 -0500

    playSound function

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ function nextSequence() {
   gamePattern.push(chosenColor);
 
   $("#" + chosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-
+  playSound(chosenColor);
 }
 $(".btn").on("click", function(){
   var userColor = this.id;
@@ -16,11 +16,35 @@ $(".btn").on("click", function(){
 });
 
 
-function playSound(){
-  $(".btn").on("click", function(){
-      var audio = new Audio("sounds/" + chosenColor + ".mp3");
-      audio.play();
-  })
+function playSound(name){
+  switch (name) {
+    case "green":
+      var green = new Audio("sounds/green.mp3");
+      //green.play();
+      console.log("g");
+      break;
+    case "red":
+      var red = new Audio("sounds/red.mp3");
+      //red.play();
+      console.log("r");
+      break;
+    case "yellow":
+      var yellow = new Audio("sounds/yellow.mp3");
+      //yellow.play();
+      console.log("y");
+      break;
+    case "blue":
+      var blue = new Audio("sounds/blue.mp3");
+      //blue.play();
+      console.log("b");
+      break;
+    default: console.log("something is broken.");
+
+
+
+
+     }
+
 }
 
 

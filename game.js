b3f7dc6f8cb53f92a5ea700b005bfa622e7245bf
mew101e <mshwss@gmail.com>
2021-03-09 11:42:33 -0500

    refactor index.js

--- game.js
+++ game.js
@@ -10,35 +10,37 @@ function nextSequence() {
   $("#" + chosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(chosenColor);
 }
-$(".btn").on("click", function(){
-  var userColor = this.id;
-  userClickedPattern.push(userColor);
-});
+// $(".btn").on("click", function(){
+//   var userColor = this.id;
+//   userClickedPattern.push(userColor);
+// });
 
 
 function playSound(name){
-  switch (name) {
-    case "green":
-      var green = new Audio("sounds/green.mp3");
-      //green.play();
-      console.log("g");
-      break;
-    case "red":
-      var red = new Audio("sounds/red.mp3");
-      //red.play();
-      console.log("r");
-      break;
-    case "yellow":
-      var yellow = new Audio("sounds/yellow.mp3");
-      //yellow.play();
-      console.log("y");
-      break;
-    case "blue":
-      var blue = new Audio("sounds/blue.mp3");
-      //blue.play();
-      console.log("b");
-      break;
-    default: console.log("something is broken.");
+  var audio = new Audio("sounds/" + name +'.mp3');
+  audio.play();
+  // switch (name) {
+  //   case "green":
+  //     var green = new Audio("sounds/green.mp3");
+  //     //green.play();
+  //     console.log("g");
+  //     break;
+  //   case "red":
+  //     var red = new Audio("sounds/red.mp3");
+  //     //red.play();
+  //     console.log("r");
+  //     break;
+  //   case "yellow":
+  //     var yellow = new Audio("sounds/yellow.mp3");
+  //     //yellow.play();
+  //     console.log("y");
+  //     break;
+  //   case "blue":
+  //     var blue = new Audio("sounds/blue.mp3");
+  //     //blue.play();
+  //     console.log("b");
+  //     break;
+  //   default: console.log("something is broken.");
 
 
 

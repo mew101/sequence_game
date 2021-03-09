c0a8ff5d4ff1922d3a1c20286913c953c15c2cd5
mew101e <mshwss@gmail.com>
2021-03-08 20:19:18 -0500

    clcick array

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 buttonColors = ["green", "red", "yellow", "blue"];
 gamePattern = [];
+userClickedPattern = [];
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() *4);
@@ -7,9 +8,20 @@ function nextSequence() {
   gamePattern.push(chosenColor);
 
   $("#" + chosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+}
+$(".btn").on("click", function(){
+  var userColor = this.id;
+  userClickedPattern.push(userColor);
+});
+
+
+function playSound(){
   $(".btn").on("click", function(){
       var audio = new Audio("sounds/" + chosenColor + ".mp3");
       audio.play();
   })
 }
+
+
 nextSequence();
